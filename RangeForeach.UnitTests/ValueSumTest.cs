4fd2f37c94220838b4ad58d0d4a46de98193e62e
天南十字星 <zjh96@live.cn>
2022-07-03 21:58:31 +0800

    Add range-foreach syntax unit test project.

---
+++ RangeForeach.UnitTests/ValueSumTest.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static System.Linq.Enumerable;
+
+namespace RangeForeach
+{
+    [TestClass]
+    public class ValueSumTest
+    {
+        [TestMethod]
+        public void ForEachLoop_PositiveRange_GetExpectedSum()
+        {
+            var sum = 0;
+            foreach (var index in 0..100)
+            {
+                sum += index;
+            }
+            var expected = Range(0, 100).Sum();
+            Assert.AreEqual(expected, sum);
+        }
+
+        [TestMethod]
+        public void ForEachLoop_NegativeRange_GetExpectedSum()
+        {
+            var sum = 0;
+            foreach (var index in ^100..0)
+            {
+                sum += index;
+            }
+            var expected = Range(-100, 100).Sum();
+            Assert.AreEqual(expected, sum);
+        }
+
+        [TestMethod]
+        public void ForEachLoop_HybridRange_GetExpectedSum()
+        {
+            var sum = 0;
+            foreach (var index in ^100..100)
+            {
+                sum += index;
+            }
+            var expected = Range(-100, 200).Sum();
+            Assert.AreEqual(expected, sum);
+        }
+
+        [TestMethod]
+        public void ForEachLoop_InvalidRange_GetZeroSum()
+        {
+            var sum = 0;
+            foreach (var index in 100..^100)
+            {
+                sum += index;
+            }
+            var expected = 0;
+            Assert.AreEqual(expected, sum);
+        }
+
+        [TestMethod]
+        public void ForEachLoop_PositiveSteppedRange_GetExpectedSum()
+        {
+            var sum = 0;
+            foreach (var index in (^100..100).Step(2))
+            {
+                sum += index;
+            }
+            var expected = Range(-50, 100).Sum() * 2;
+            Assert.AreEqual(expected, sum);
+        }
+
+        [TestMethod]
+        public void ForEachLoop_NegativeSteppedRange_GetExpectedSum()
+        {
+            var sum = 0;
+            foreach (var index in (99..^101).Step(-1))
+            {
+                sum += index;
+            }
+            var expected = Range(-50, 100).Sum() * 2;
+            Assert.AreEqual(expected, sum);
+        }
+    }
+}
