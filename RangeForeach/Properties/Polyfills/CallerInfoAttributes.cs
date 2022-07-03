12eff6b60217ae009df44aafe5e12564e4ef964a
天南十字星 <zjh96@live.cn>
2022-07-03 21:56:42 +0800

    Add range-foreach syntax source and polyfills.

---
+++ RangeForeach/Properties/Polyfills/CallerInfoAttributes.cs
@@ -0,0 +1,102 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+#pragma warning disable
+
+#if !(CALLER_INFO_ATTRIBUTES || NET45_OR_GREATER || NETCOREAPP || NETSTANDARD)
+namespace System.Runtime.CompilerServices
+{
+    using System.Diagnostics;
+    using System.Diagnostics.CodeAnalysis;
+
+    /// <summary>
+    /// Allows you to obtain the full path of the source file that contains the caller.
+    /// This is the file path at the time of compile.
+    /// </summary>
+    [DebuggerNonUserCode, ExcludeFromCodeCoverage]
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class CallerFilePathAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CallerFilePathAttribute"/> class.
+        /// </summary>
+        public CallerFilePathAttribute()
+        {
+        }
+    }
+
+    /// <summary>
+    /// Allows you to obtain the line number in the source file at which the method is called.
+    /// </summary>
+    [DebuggerNonUserCode, ExcludeFromCodeCoverage]
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class CallerLineNumberAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CallerLineNumberAttribute"/> class.
+        /// </summary>
+        public CallerLineNumberAttribute()
+        {
+        }
+    }
+
+    /// <summary>
+    /// Allows you to obtain the method or property name of the caller to the method.
+    /// </summary>
+    [DebuggerNonUserCode, ExcludeFromCodeCoverage]
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class CallerMemberNameAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CallerMemberNameAttribute"/> class.
+        /// </summary>
+        public CallerMemberNameAttribute()
+        {
+        }
+    }
+}
+#endif
+
+#if !(CALLER_INFO_ATTRIBUTES || NETCOREAPP3_0_OR_GREATER)
+namespace System.Runtime.CompilerServices
+{
+    using System.Diagnostics;
+    using System.Diagnostics.CodeAnalysis;
+
+    /// <summary>
+    /// Allows capturing of the expressions passed to a method.
+    /// </summary>
+    [DebuggerNonUserCode, ExcludeFromCodeCoverage]
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    internal sealed class CallerArgumentExpressionAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CallerArgumentExpressionAttribute"/> class.
+        /// </summary>
+        /// <param name="parameterName">The name of the targeted parameter.</param>
+        public CallerArgumentExpressionAttribute(string parameterName)
+        {
+            this.ParameterName = parameterName;
+        }
+
+        /// <summary>
+        /// Gets the target parameter name of the CallerArgumentExpression.
+        /// </summary>
+        /// <returns>The name of the targeted parameter of the CallerArgumentExpression.</returns>
+        public string ParameterName { get; }
+    }
+}
+#endif
+
+#if !(EXCLUDE_FROM_CODE_COVERAGE_ATTRIBUTE || NETCOREAPP3_0_OR_GREATER)
+#if !(NET40_OR_GREATER || NETCOREAPP2_0_OR_GREATER || NETSTANDARD2_0_OR_GREATER)
+namespace System.Diagnostics.CodeAnalysis
+{
+    // Excludes the attributed code from code coverage information.
+    internal sealed partial class ExcludeFromCodeCoverageAttribute : Attribute
+    {
+    }
+}
+#endif
+#endif
---
+++ RangeForeach/Properties/RangeForeach.cs
@@ -0,0 +1,128 @@
+﻿// Copyright (c) 2022 XstarS
+// This file is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+// Provides the range-foreach syntax for C# 9.0 or higher.
+// Requires: System.Index struct, System.Range struct.
+// Reference this file to write foreach-loops like this:
+//   foreach (var index in 0..100) { }
+// which is equivalent to the legacy for-loop below:
+//   for (int index = 0; index < 100; index++) { }
+// NOTE: Use '^' to represent negative numbers,
+//       e.g. ^100..0 (instead of -100..0).
+// If STEPPED_RANGE is defined, this can also be used:
+//   foreach (var index in (99..^1).Step(-2)) { }
+// which is equivalent to the legacy for-loop below:
+//   for (int index = 99; index >= 0; index -= 2) { }
+
+#nullable disable
+#pragma warning disable
+#define STEPPED_RANGE
+
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+[DebuggerNonUserCode, ExcludeFromCodeCoverage]
+[EditorBrowsable(EditorBrowsableState.Never)]
+[Obsolete("This type supports the range-foreach syntax " +
+          "and should not be used directly in user code.")]
+internal static class RangeEnumerable
+{
+    public static Enumerator GetEnumerator(this Range range)
+    {
+        return new Enumerator(in range);
+    }
+
+    [DebuggerNonUserCode, ExcludeFromCodeCoverage]
+    public struct Enumerator
+    {
+        private int CurrentIndex;
+
+        private readonly int EndIndex;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal Enumerator(in Range range)
+        {
+            this.CurrentIndex = range.Start.GetOffset(0) - 1;
+            this.EndIndex = range.End.GetOffset(0);
+        }
+
+        public int Current => this.CurrentIndex;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool MoveNext() => ++this.CurrentIndex < this.EndIndex;
+    }
+
+#if STEPPED_RANGE
+    public static Stepped Step(this Range range, int step)
+    {
+        return new Stepped(range, step);
+    }
+
+    [DebuggerNonUserCode, ExcludeFromCodeCoverage]
+    public readonly struct Stepped
+    {
+        public readonly Range Range;
+
+        public readonly int Step;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Stepped(Range range, int step)
+        {
+            if (step == 0) { throw Stepped.StepOutOfRange(); }
+            this.Range = range; this.Step = step;
+        }
+
+        public Enumerator GetEnumerator() => new Enumerator(in this);
+
+        private static ArgumentOutOfRangeException StepOutOfRange() =>
+            new ArgumentOutOfRangeException("step", "Non-zero number required.");
+
+        [DebuggerNonUserCode, ExcludeFromCodeCoverage]
+        public struct Enumerator
+        {
+            private int CurrentIndex;
+
+            private readonly int EndIndex;
+
+            private readonly int StepSign;
+
+            private readonly int StepValue;
+
+            internal Enumerator(in Stepped stepped)
+            {
+                var range = stepped.Range;
+                int start = range.Start.GetOffset(0);
+                int end = range.End.GetOffset(0);
+                int step = stepped.Step;
+                int sign = step >> 31;
+                this.CurrentIndex = (start - step) ^ sign;
+                this.EndIndex = end ^ sign;
+                this.StepSign = sign;
+                this.StepValue = (step ^ sign) - sign;
+            }
+
+            public int Current => this.CurrentIndex ^ this.StepSign;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool MoveNext() =>
+                (this.CurrentIndex += this.StepValue) < this.EndIndex;
+        }
+    }
+#endif
+}
+
+#if !(EXCLUDE_FROM_CODE_COVERAGE_ATTRIBUTE || NETCOREAPP3_0_OR_GREATER)
+#if !(NET40_OR_GREATER || NETCOREAPP2_0_OR_GREATER || NETSTANDARD2_0_OR_GREATER)
+namespace System.Diagnostics.CodeAnalysis
+{
+    // Excludes the attributed code from code coverage information.
+    internal sealed partial class ExcludeFromCodeCoverageAttribute : Attribute
+    {
+    }
+}
+#endif
+#endif
