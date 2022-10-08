26b6663688438e15215a9edc49f6f8a176e4030a
天南十字星 <zjh96@live.cn>
2022-10-08 22:50:03 +0800

    Add range-foreach instantiation unit test cases.

---
+++ RangeForeach.UnitTests/InstantiationTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RangeForeach
+{
+    [TestClass]
+    public class InstantiationTest
+    {
+        [TestMethod]
+        public void GetEnumerator_StartEqualToEnd_DoNothing()
+        {
+            foreach (var index in ..)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void GetEnumerator_StartGreaterThanEnd_DoNothing()
+        {
+            foreach (var index in 10..0)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void GetEnumerator_NegativeIndex_CatchOutOfRangeException()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => (..-1).GetEnumerator());
+        }
+
+        [TestMethod]
+        public void SteppedRange_ZeroStep_CatchOutOfRangeException()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => (0..10).Step(0).GetEnumerator());
+        }
+    }
+}
